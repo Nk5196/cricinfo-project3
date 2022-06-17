ac0ca7e06b16282c2811e4dc3fe1619328a4a7f7
your github user name <narendrakandalkar@gmail.com>
2022-06-17 09:22:37 +0530

    footer linked to the all files

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 import scorecorousel from "./component/scorecorousel.js";
 
  document.getElementById("midbar").innerHTML = scorecorousel()
+
+ import footercompo from "./component/footer.js";
+
+ document.getElementById("footer").innerHTML = footercompo()
---
+++ livescore.js
@@ -0,0 +1,3 @@
+import footercompo from "./component/footer.js";
+
+ document.getElementById("footer").innerHTML = footercompo()
